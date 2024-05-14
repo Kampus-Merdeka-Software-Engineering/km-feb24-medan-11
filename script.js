6f2eabde3d00626999cddc674f44cb1e30c02f33
farhanari <farhanarinw@gmail.com>
2024-05-14 16:53:15 +0700

    Add Sidebar navigation + Javascript File

---
+++ script.js
@@ -0,0 +1,18 @@
+const body = document.querySelector("body"),
+    sidebar = body.querySelector(".sidebar"),
+    toggle = body.querySelector(".toggle"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+});
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        modeText.innerText = "Light Mode";
+    } else {
+        modeText.innerText = "Dark Mode";
+    }
+});
